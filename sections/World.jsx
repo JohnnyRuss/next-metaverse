d38086eda53261be963ac26df9734eee5d9651b0
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-09-05 12:42:01 +0400

    ready to deploy

--- sections/World.jsx
+++ sections/World.jsx
@@ -49,6 +49,84 @@ const World = () => {
           <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
             <img src="/people-03.webp" alt="people" className="w-full h-full" />
           </div>
+
+          <div className="absolute top-[40%] left-[20%] w-[217px] h-[167px] p-[10px] rounded-[20px] bg-[#5d6680] flex items-center justify-center">
+            <div className="w-[90%] h-[90%] bg-pink-500 blur-[20px] absolute z-0" />
+            <div className="relative z-1 w-full h-full">
+              <img
+                src="/upside-down.png"
+                alt="people"
+                className="w-full h-full rounded-[20px] object-cover"
+              />
+
+              <div className="absolute bottom-0 flex flex-col gap-1 p-3 w-full">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center">
+                    <img
+                      src="/people-01.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full relative z-[2]"
+                    />
+                    <img
+                      src="/people-02.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full -ml-[12px] relative z-[1]"
+                    />
+                    <img
+                      src="/people-03.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full -ml-[12px]"
+                    />
+                  </div>
+                  <span className="-ml-[12px] text-[12px] flex items-center text-white">
+                    + 264 has joined
+                  </span>
+                </div>
+                <h4 className="text-[18px] text-white font-extrabold">
+                  The Upside Down
+                </h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="absolute top-[7%] right-[20%] w-[217px] h-[167px] p-[10px] rounded-[20px] bg-[#5d6680] flex items-center justify-center">
+            <div className="w-[90%] h-[90%] bg-indigo-500 blur-[20px] absolute z-0" />
+            <div className="relative z-1 w-full h-full">
+              <img
+                src="/hawkins-hub.png"
+                alt="people"
+                className="w-full h-full rounded-[20px] object-cover"
+              />
+
+              <div className="absolute bottom-0 flex flex-col gap-1 p-3 w-full">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center">
+                    <img
+                      src="/people-03.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full relative z-[2]"
+                    />
+                    <img
+                      src="/people-02.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full -ml-[12px] relative z-[1]"
+                    />
+                    <img
+                      src="/people-01.webp"
+                      alt="people"
+                      className="w-[22პხ] h-[22px] rounded-full -ml-[12px]"
+                    />
+                  </div>
+                  <span className="-ml-[12px] text-[12px] flex items-center text-white">
+                    + 264 has joined
+                  </span>
+                </div>
+                <h4 className="text-[18px] text-white font-extrabold">
+                  Hawkins Labs
+                </h4>
+              </div>
+            </div>
+          </div>
         </motion.div>
       </motion.div>
     </section>
